c6d3ee3cb3a46f6fff27c9362967ffe84d694547
Swagatam <swagatamheroz@outlook.com>
2025-04-16 17:45:55 +0530

    fix: routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import routes from "./routes.jsx";
+import App from "./App.jsx";
+
+import { AuthProvider } from "./contexts/auth-context.jsx";
 
 import "./index.css";
 
-const router = createBrowserRouter(routes);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
--- frontend/src/routes.jsx
+++
@@ -1,5 +0,0 @@
-import App from "./App.jsx";
-
-const routes = [{ path: "/", element: <App /> }];
-
-export default routes;
