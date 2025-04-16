35e4cad7b8537ed3bff2fcc5f3d453e2bfbf6f8a
Swagatam <swagatamheroz@outlook.com>
2025-04-16 17:46:19 +0530

    feat: add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
-import { Button } from "@/ui/Button.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { useAuth } from "./contexts/auth-context.jsx";
+
+import HomePage from "./pages/HomePage.jsx";
+import JourneyDetailsPage from "./pages/JourneyDetailsPage.jsx";
+import SignIn from "./pages/SignInPage.jsx";
+import SignUp from "./pages/SignUpPage.jsx";
+import ReserveTicketsPage from "./pages/ReserveTicketsPage.jsx";
 
 export default function App() {
+  const { user } = useAuth();
+
   return (
-    <Button variant="outline">Hermes</Button>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/journey-details" element={<JourneyDetailsPage />} />
+      <Route path="/reserve-tickets" element={<ReserveTicketsPage />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="*" element={<Navigate to={user ? "/" : "/sign-in"} />} />
+    </Routes>
   );
 }
